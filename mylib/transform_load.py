8a911171a17e3e7886afff6e9033f082b4ebcd9b
Eric Ortega <ericiortega85@gmail.com>
2024-10-05 12:57:49 -0400

    fixing transform

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -21,16 +21,33 @@ def load(dataset="data/avengers.csv", db_name="avengers.db", table_name="Avenger
         c.execute(f"DROP TABLE IF EXISTS {table_name}")
         c.execute(f"""
         CREATE TABLE {table_name} (
+            URL TEXT,
+            Name_Alias TEXT,
             Appearances INTEGER, 
             Current TEXT, 
-            Gender TEXT, 
-            Year INTEGER
+            Gender TEXT,
+            Probationary_Introl TEXT,
+            Full_Reserve_Avengers_Intro TEXT,
+            Year INTEGER,
+            Years_since_joining INTEGER,
+            Honorary TEXT,
+            Death1 TEXT,
+            Return1 TEXT,
+            Death2 TEXT,
+            Return2 TEXT,
+            Death3 TEXT,
+            Return3 TEXT,
+            Death4 TEXT,
+            Return4 TEXT,
+            Death5 TEXT,
+            Return5 TEXT,
+            Notes TEXT
         )
         """)
         
         # Skip the header and insert the data into the table
         next(payload)  # Skip header row
-        c.executemany(f"INSERT INTO {table_name} VALUES (?,?,?,?,?)", payload)
+        c.executemany(f"INSERT INTO {table_name} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", payload)
         
         # Commit the changes and close the connection
         conn.commit()
@@ -39,5 +56,5 @@ def load(dataset="data/avengers.csv", db_name="avengers.db", table_name="Avenger
     return db_name
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     load()
